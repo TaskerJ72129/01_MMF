d3fbb29ffbabfe3c83c22ea044c93f4e75352888
TaskerJ72129 <60583303+TaskerJ72129@users.noreply.github.com>
2021-02-26 12:20:37 +1300

    thing

--- 00_MMF_base_V2.py
+++ 00_MMF_base_V2.py
@@ -41,14 +41,42 @@ def int_check(question):
             print(error)
 
 
-# checks number of tickets left and warns user if macimum is being approached
+# checks number of tickets left and warns user if maximum is being approached
 def check_tickets(tickets_sold, ticket_limit):
     # tells user how many seats are left
     if tickets_sold < ticket_limit - 1:
-        print("You have {} seats"
+        print("You have {} seats "
               "left".format(ticket_limit - tickets_sold))
 
+    # warns user only one seat is left!
+    else:
+        print("*** There is ONE seat left!! ***")
 
+    return ""
+
+
+# Gets ticket price based on age
+def get_ticket_price():
+
+    # Get age (between 12 and 130)
+    age = int_check("Age: ")
+
+    # check that age is valid
+    if age < 12:
+        print("Sorry you are too young for this movie")
+        return "invalid ticket price"
+    elif age > 130:
+        print("That is very old it looks like a mistake")
+        return "invalid ticket price"
+
+    if age < 16:
+        ticket_price = 7.5
+    elif age < 65:
+        ticket_price = 10.5
+    else:
+        ticket_price = 6.5
+
+    return ticket_price
 # ----- Main Routine -----
 
 # dictionaries / lists
@@ -62,19 +90,27 @@ name = ""
 ticket_count = 0
 ticket_sales = 0
 
+# Initialise lists (to make data-frame in due course)
+all_names = []
+all_tickets = []
 
+
+# Data Frame Dictionary
+movie_data_dict = {
+    'Name': all_names,
+    'Ticket': all_tickets
+}
+
+
+# Ask user if they have used program before & show instructions
+
+# Loop to get ticket details
 while name != "xxx" and ticket_count < MAX_TICKETS:
 
-    # tells user how many seats left
-    if ticket_count < MAX_TICKETS - 1:
-        print("You have {} seats "
-              "left".format(MAX_TICKETS - ticket_count))
+    # check numbers of ticket limit has not been exceeded
+    check_tickets(ticket_count, MAX_TICKETS)
 
-    # warns user only one seat is left!
-    else:
-        print("*** There is ONE seat left!! ***")
-
-    # Get details...
+    # Get details for each ticket
 
     # Get name (no blank)
     name = not_blank("Name: ")
@@ -84,28 +120,27 @@ while name != "xxx" and ticket_count < MAX_TICKETS:
         break
 
     # Get age (between 12 and 130)
-    age = int_check("Age: ")
-
-    # check that age is valid..
-    if age < 12:
-        print("Sorry you are too young for this movie")
+    ticket_price = get_ticket_price()
+    # if age is invalid, restart loop (and get name again
+    if ticket_price == "invalid ticket price":
         continue
-    elif age > 130:
-        print("That is very old it looks like a mistake")
-        continue
-
-    if age < 16:
-        ticket_price = 7.5
-    elif age < 65:
-        ticket_price = 10.5
-    else:
-        ticket_price = 6.5
 
     ticket_count += 1
     ticket_sales += ticket_price
 
+    # add name and ticket price to lists
+    all_names.append(name)
+    all_tickets.append(ticket_price)
 
-# End of tickets loop
+    # Get snacks
+
+    # Get payment method (ie: work out if surcharge is needed)
+
+# End of tickets loop / snacks / payment loop
+
+# print details
+movie_frame = pandas.DataFrame(movie_data_dict)
+print(movie_frame)
 
 # Calculate ticket profit
 ticket_profit = ticket_sales - (5 * ticket_count)
