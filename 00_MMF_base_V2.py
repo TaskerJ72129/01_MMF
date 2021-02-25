98ae5b541cf51a7803ae82730e80ac0b0248db4b
TaskerJ72129 <60583303+TaskerJ72129@users.noreply.github.com>
2021-02-25 13:19:13 +1300

    V6,7 and some base V2

---
+++ 00_MMF_base_V2.py
@@ -0,0 +1,129 @@
+# import statements
+import re
+import pandas
+
+
+# functions go here
+
+# checks that ticket name is not blank
+def not_blank(question):
+    valid = False
+
+    while not valid:
+        response = input(question)
+
+        if response != "":
+            return response
+        else:
+            print("Sorry - this can't be blank, "
+                  "please enter your name.")
+
+
+# checks for an integer between two values
+def int_check(question):
+
+    error = "Please enter a whole number"
+
+    valid = False
+    while not valid:
+
+        # ask user for number and check it is valid
+        try:
+            response = int(input(question))
+
+            if response < 0:
+                print(error)
+            else:
+                return response
+
+        # if an integer is not entered, display an error
+        except ValueError:
+            print(error)
+
+
+# checks number of tickets left and warns user if macimum is being approached
+def check_tickets(tickets_sold, ticket_limit):
+    # tells user how many seats are left
+    if tickets_sold < ticket_limit - 1:
+        print("You have {} seats"
+              "left".format(ticket_limit - tickets_sold))
+
+
+# ----- Main Routine -----
+
+# dictionaries / lists
+
+# used program before?
+
+# loop
+MAX_TICKETS = 5
+
+name = ""
+ticket_count = 0
+ticket_sales = 0
+
+
+while name != "xxx" and ticket_count < MAX_TICKETS:
+
+    # tells user how many seats left
+    if ticket_count < MAX_TICKETS - 1:
+        print("You have {} seats "
+              "left".format(MAX_TICKETS - ticket_count))
+
+    # warns user only one seat is left!
+    else:
+        print("*** There is ONE seat left!! ***")
+
+    # Get details...
+
+    # Get name (no blank)
+    name = not_blank("Name: ")
+
+    # end the loop if the exit code is entered
+    if name == "xxx":
+        break
+
+    # Get age (between 12 and 130)
+    age = int_check("Age: ")
+
+    # check that age is valid..
+    if age < 12:
+        print("Sorry you are too young for this movie")
+        continue
+    elif age > 130:
+        print("That is very old it looks like a mistake")
+        continue
+
+    if age < 16:
+        ticket_price = 7.5
+    elif age < 65:
+        ticket_price = 10.5
+    else:
+        ticket_price = 6.5
+
+    ticket_count += 1
+    ticket_sales += ticket_price
+
+
+# End of tickets loop
+
+# Calculate ticket profit
+ticket_profit = ticket_sales - (5 * ticket_count)
+print("Ticket profit: ${:.2f}".format(ticket_profit))
+
+# Tell user if they have unsold tickets
+if ticket_count == MAX_TICKETS:
+    print("You have sold all the available tickets!")
+else:
+    print("You have sold {} tickets.  \n"
+          "There are {} places still available"
+          .format(ticket_count, MAX_TICKETS - ticket_count))
+
+
+
+
+
+    # price
+
+
+    # loop snack
---
+++ 06_string_checker_V7.py
@@ -0,0 +1,120 @@
+import re
+
+
+# Function goes here
+def string_check(choice, options):
+
+    for var_list in options:
+
+        # if the snack is in one of the lists, return the full name
+        if choice in var_list:
+
+            # Get full name of snack and put it
+            # in title case so it looks nice when outputted
+            chosen = var_list[0].title()
+            is_valid = "yes"
+            break
+
+        # if the chosen option is not valid, set in_valid to no
+        else:
+            is_valid = "no"
+
+    # if the snacks is not OK - ask question again.
+    if is_valid == "yes":
+        return chosen
+    else:
+        print("Please enter a valid option\n")
+        return "invalid choice"
+
+
+# Gets list of snacks
+def get_snack():
+    # regular expression to find if item starts with a number
+    number_regex = "^[1-9]"
+
+    # valid snacks holds list of all snacks
+    # Each item in valid snacks is a list with valid options for each snack
+    # <full name, letter code (a - e), and possible abbreviations etc>
+    valid_snacks = [
+    ["popcorn", "p", "corn", "a"],
+    ["M&M's", "m&m's", "mms", "m", "b"],
+    ["pita chips", "chips", "pc", "pita", "c"],
+    ["water", "w", "d"],
+    ["orange juice", "oj", "o", "juice", "e"]
+    ]
+
+    # holds snack order for a single user
+    snack_order = []
+
+    desired_snack = ""
+    while desired_snack != "xxx":
+
+        snack_row = []
+
+        # ask user for desired snack and put it in lowercase
+        desired_snack = input("Snack: ").lower()
+
+        if desired_snack == "xxx":
+            return snack_order
+
+        # if item has a number, separate it into two (number / item)
+        if re.match(number_regex, desired_snack):
+            amount = int(desired_snack[0])
+            desired_snack = desired_snack[1:]
+
+        else:
+            amount = 1
+            desired_snack = desired_snack
+
+
+        # remove white space around snack
+        desired_snack = desired_snack.strip()
+
+        # check if snack is valid
+        snack_choice = string_check(desired_snack, valid_snacks)
+        '''print("Snack Choice: ", snack_choice)'''
+
+        # check snack amount is valid (less than 5)
+        if amount >= 5:
+            print("Sorry - we have a four snack maximum")
+            snack_choice = "invalid choice"
+
+        # add snack AND amount to list...
+
+        snack_row.append(amount)
+        snack_row.append(snack_choice)
+
+        # check that snack is not the exit code before adding
+        if snack_choice != "xxx" and snack_choice != "invalid choice":
+            snack_order.append(snack_row)
+
+
+
+# valid options for yes / no questions
+yes_no = [
+    ["yes", "y"],
+    ["no", "n"]
+]
+
+# ask user if they want a snack
+check_snack = "invalid choice"
+while check_snack == "invalid choice":
+    want_snack = input("Do you want to order snacks? ").lower()
+    check_snack = string_check(want_snack, yes_no)
+
+# If they say yes, ask what snacks they want (and add to snack list)
+if check_snack == "Yes":
+    get_order = get_snack()
+
+else:
+    get_order = []
+
+# Show snack orders
+print()
+if len(get_order) == 0:
+    print("Snacks Ordered: None")
+
+else:
+    print("Snacks Ordered:")
+
+    print(get_order)
